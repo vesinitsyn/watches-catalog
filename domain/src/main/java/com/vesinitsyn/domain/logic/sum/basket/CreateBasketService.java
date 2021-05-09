ea7b2b11f49a19e924528ac1d38eff3dd90848e7
vesinitsyn <oldgrezli@mail.ru>
2021-05-09 10:24:37 +0300

    basket creation service implementation

--- domain/src/main/java/com/vesinitsyn/domain/logic/sum/basket/CreateBasketService.java
+++ domain/src/main/java/com/vesinitsyn/domain/logic/sum/basket/CreateBasketService.java
@@ -1,10 +1,18 @@
 package com.vesinitsyn.domain.logic.sum.basket;
 
 import com.vesinitsyn.domain.model.Basket;
+import com.vesinitsyn.domain.model.Watch;
 import com.vesinitsyn.infrastructure.WatchesStorage;
 import lombok.RequiredArgsConstructor;
 
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import static com.vesinitsyn.domain.model.Basket.emptyBasket;
+import static java.util.function.Function.identity;
+import static java.util.stream.Collectors.*;
 
 @RequiredArgsConstructor
 public class CreateBasketService {
@@ -12,6 +20,19 @@ public class CreateBasketService {
     private final WatchesStorage watchesStorage;
 
     public Basket createBasket(List<String> purchasedWatchIds) {
-        throw new UnsupportedOperationException();
+        if (purchasedWatchIds == null || purchasedWatchIds.isEmpty()) {
+            return emptyBasket();
+        }
+        Map<String, Long> quantityByWatchId = purchasedWatchIds.stream()
+                .collect(groupingBy(identity(), counting()));
+        Set<String> uniqueWatchIds = quantityByWatchId.keySet();
+        Set<Watch> watches = watchesStorage.findByIds(uniqueWatchIds);
+        Map<Watch, BigDecimal> quantityByWatch = watches.stream()
+                .collect(toMap(
+                        identity(),
+                        watch -> BigDecimal.valueOf(quantityByWatchId.get(watch.getId()))
+                        )
+                );
+        return new Basket(quantityByWatch);
     }
 }
